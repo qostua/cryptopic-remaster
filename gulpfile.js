aa06c20cbc149646998f234cb09196c7d36de677
qostua <qostua@gmail.com>
2022-05-21 15:24:10 +0300

    Переработал front

---
+++ gulpfile.js
@@ -0,0 +1,150 @@
+// process.env.DISABLE_NOTIFIER = true;
+
+const {src, dest, parallel, series, watch} = require('gulp');
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+const htmlmin = require('gulp-htmlmin');
+const webpHtml = require('gulp-webp-html-fix');
+const retinaHtml = require('gulp-img-retina');
+
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+const svgSprite = require('gulp-svg-sprite');
+const newer = require('gulp-newer');
+
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
+
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
+const scripts = () => {
+	return src([
+		'src/js/main.js'
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('app.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(dest('src/js/'))
+	.pipe(browserSync.stream())
+}
+const scriptsLibs = () => {
+	return src([
+		'src/js/libs.js',
+	])
+	.pipe(sourcemaps.init())
+	.pipe(concat('app-libs.js'))
+	.pipe(sourcemaps.write('.'))
+	.pipe(dest('src/js/'))
+	.pipe(browserSync.stream())
+}
+const htmlPicture = () => {
+	return src('src/*.html')
+	.pipe(retinaHtml({
+		suffix: {1: '', 2: '@2x'}
+	}))
+	.pipe(webpHtml())
+	.pipe(dest('src/'))
+}
+const styles = () => {
+  return src('src/scss/main.scss')
+    .pipe(sourcemaps.init())
+		.pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'expanded' })
+		.on('error', notify.onError()))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('src/css/'))
+    .pipe(browserSync.stream());
+}
+const svg = () => {
+	return src('src/img/src/*.svg')
+		.pipe(newer('src/img/'))
+		.pipe(dest('src/img/'));
+}
+const sprite = () => {
+	return src('src/img/sprite/*.svg')
+		.pipe(svgSprite({ mode: { stack: { sprite: "../sprite.svg" } } }))
+		.pipe(dest('src/img/'));
+}
+const fonts = () => {
+  src('src/fonts/src/*.ttf')
+		.pipe(newer({dest: 'src/fonts/', ext: '.woff'}))
+		.pipe(ttf2woff())
+		.pipe(dest('src/fonts'))
+	return src('src/fonts/src/*.ttf')
+		.pipe(newer({dest: 'src/fonts/', ext: '.woff2'}))
+		.pipe(ttf2woff2())
+		.pipe(dest('src/fonts/'))
+}
+const watchFiles = () => {
+  browserSync.init({
+        server: { baseDir: "./src" },
+        cors: true,
+        notify: false,
+        ui: false,
+    });
+    watch('src/*.html').on('change', browserSync.reload);
+		watch('src/scss/**/*.scss', styles);
+		watch('src/img/src/*.svg', svg);
+    watch('src/img/sprite/*.svg', sprite);
+		watch('src/fonts/src/*.{ttf,woff,woff2}', fonts);
+		watch(['src/**/*.js', '!src/**/app*.*'], scripts);
+		watch(['libs/**/*.js'], scriptsLibs);
+}
+
+const cleanSrc = () => {
+	return del(['src/css/*.*', 'src/fonts/*.*', 'src/img/*.*', 'src/js/*.map', 'src/js/app*.js'])
+}
+
+exports.default = parallel(styles, svg, sprite, scripts, scriptsLibs, fonts, watchFiles);
+exports.htmlPicture = htmlPicture;
+exports.cleanSrc = cleanSrc;
+
+const htmlBuild = () => {
+	return src('src/*.html')
+		//.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(dest('build'))
+}
+const stylesBuild = () => {
+  return src('./src/css/main.css')
+    //.pipe(cleanCSS({ level: 2 }))
+    .pipe(dest('build/css/'));
+}
+const svgBuild = () => {
+	return src('src/img/*.svg')
+		.pipe(dest('build/img'));
+}
+
+const fontsBuild = () => {
+	return src('src/fonts/*.{woff,woff2}')
+	.pipe(dest('build/fonts'))
+}
+const resourcesBuild = () => {
+  return src('src/resources/**/*')
+    .pipe(dest('build/resources'))
+}
+const scriptsBuild = () => {
+	return src('src/js/app.js')
+	//.pipe(uglify())
+	.pipe(dest('build/js/'))
+}
+const scriptsLibsBuild = () => {
+	return src('src/js/app-libs.js')
+	//.pipe(uglify())
+	.pipe(dest('build/js/'))
+}
+
+const cleanBuild = () => {
+	return del('build/*')
+}
+
+exports.build = series(cleanBuild, parallel(htmlBuild, stylesBuild, svgBuild, scriptsBuild, scriptsLibsBuild, resourcesBuild, fontsBuild));
+exports.cleanBuild = cleanBuild;
---
+++ src/js/main.js
@@ -0,0 +1,224 @@
+let encryption = document.querySelector(".encryption");
+
+let encryptionToggle = document.querySelector(".encryption_toggle");
+
+encryption.classList.remove("encryption--encrypt");
+encryption.classList.remove("encryption--decipher");
+
+if (encryptionToggle.checked) {
+  encryption.classList.add("encryption--decipher");
+} else {
+  encryption.classList.add("encryption--encrypt");
+}
+
+encryptionToggle.addEventListener("click", function() {
+  encryption.classList.remove("encryption--encrypt");
+  encryption.classList.remove("encryption--decipher");
+
+  if (encryptionToggle.checked) {
+    encryption.classList.add("encryption--decipher");
+  } else {
+    encryption.classList.add("encryption--encrypt");
+  }
+});
+
+
+
+let encryptionData = {
+  addPicture(data, width, height, type, count = "") {
+    this["picture" + count] = {
+      data: data,
+      width: width,
+      height: height,
+      type: type,
+    }
+  },
+  addMessage(text) {
+    this.messege = text;
+  },
+  addOperationType(type) {
+    this.operationType = type;
+  },
+}
+let formEncrypt = document.querySelector(".encryption_form--encrypt");
+
+let encryptBtnNext = formEncrypt.querySelector(".encryption_form-btn--next");
+let encryptBtnBack = formEncrypt.querySelector(".encryption_form-btn--back");
+
+let formEncryptSteps = formEncrypt.querySelectorAll(".encryption_form-step");
+let formEncryptStepsArray = Array.from(formEncryptSteps);
+
+let encryptPic = formEncrypt.querySelector(".encryption_form-encrypt-pic");
+
+let encryptGoNextStep = function () {
+  if (formEncrypt.dataset.stepCount == 1) {
+    formEncrypt.dataset.stepCount = +formEncrypt.dataset.stepCount + 1;
+  } else if (formEncrypt.dataset.stepCount == 2) {
+    let cookie = document.cookie;
+    let csrfToken = cookie.substring(cookie.indexOf('=') + 1);
+    fetch(
+      // '/req',
+      'https://jsonplaceholder.typicode.com/posts',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With' : 'XMLHttpRequest',
+          'X-CSRFToken': csrfToken
+        },
+        body: JSON.stringify(encryptionData),
+      })
+      .then((response) => response.json())
+      .then((json) => encryptPic.src = json.picture.data);
+      formEncrypt.dataset.stepCount = +formEncrypt.dataset.stepCount + 1;
+  }
+}
+let encryptGoBackStep = function () {
+  formEncrypt.dataset.stepCount = +formEncrypt.dataset.stepCount - 1;
+}
+
+encryptBtnNext.addEventListener("click", encryptGoNextStep);
+encryptBtnBack.addEventListener("click", encryptGoBackStep);
+
+let formEncryptInputFile = formEncrypt.querySelector(".encryption_form-picture-input");
+let formEncryptInputText = formEncrypt.querySelector(".encryption_form-text-input");
+let formEncryptImage = formEncrypt.querySelector(".encryption_form-downloaded-pic");
+
+let toggleDownloadedPic = function(form, countPic = "") {
+  form.classList.add(`encryption_form--downloaded-pic${countPic}`);
+}
+
+let convertBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
+
+let uploadImage = async (event, image) => {
+  const file = event.target.files[0];
+  const base64 = await convertBase64(file);
+  image.src = base64;
+};
+
+let declareDataEncrypt = function(image, messege) {
+  let imageWidth = image.naturalWidth;
+  let imageHeight = image.naturalHeight;
+  let imageType = image.src.slice(image.src.indexOf("/") + 1, image.src.indexOf(";"));
+
+  encryptionData.addPicture(image.src, imageWidth, imageHeight, imageType);
+  encryptionData.addMessage(messege);
+  encryptionData.addOperationType("encrypt");
+}
+
+formEncryptInputFile.addEventListener("change", (e) => {
+  uploadImage(e, formEncryptImage);
+  toggleDownloadedPic(formEncrypt);
+  formEncryptImage.onload = function() {
+    declareDataEncrypt(formEncryptImage, formEncryptInputText.value);
+  };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let formDecipher = document.querySelector(".encryption_form--decipher");
+
+let decipherBtnNext = formDecipher.querySelector(".encryption_form-btn--next");
+let decipherBtnBack = formDecipher.querySelector(".encryption_form-btn--back");
+let formDecipherSteps = formDecipher.querySelectorAll(".encryption_form-step");
+let formDecipherStepsArray = Array.from(formDecipherSteps);
+
+let encryptText = formDecipher.querySelector(".encryption_form-text-input");
+
+let decipherGoNextStep = function () {
+  if (formDecipher.dataset.stepCount == 1) {
+    formDecipher.dataset.stepCount = +formDecipher.dataset.stepCount + 1;
+  } else if (formDecipher.dataset.stepCount == 2) {
+    let cookie = document.cookie;
+    let csrfToken = cookie.substring(cookie.indexOf('=') + 1);
+    fetch(
+      // '/req',
+      'https://jsonplaceholder.typicode.com/posts',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With' : 'XMLHttpRequest',
+          'X-CSRFToken': csrfToken
+        },
+        body: JSON.stringify(encryptionData),
+      })
+      .then((response) => response.json())
+      .then((json) => encryptText.value = json.messege);
+      formDecipher.dataset.stepCount = +formDecipher.dataset.stepCount + 1;
+  }
+}
+let decipherGoBackStep = function () {
+  formDecipher.dataset.stepCount = +formDecipher.dataset.stepCount - 1;
+}
+
+decipherBtnNext.addEventListener("click", decipherGoNextStep);
+decipherBtnBack.addEventListener("click", decipherGoBackStep);
+
+let formDecipherInputFileFirst = formDecipher.querySelector(".encryption_form-step[data-step='1'] .encryption_form-picture-input");
+let formDecipherImageFirst = formDecipher.querySelector(".encryption_form-step[data-step='1'] .encryption_form-downloaded-pic");
+
+let formDecipherInputFileSecond = formDecipher.querySelector(".encryption_form-step[data-step='2'] .encryption_form-picture-input");
+let formDecipherImageSecond = formDecipher.querySelector(".encryption_form-step[data-step='2'] .encryption_form-downloaded-pic");
+
+let declareDataDecipher = function(imageFirst, imageSecond) {
+  encryptionData.addOperationType("decipher");
+
+  let imageFirstWidth = imageFirst.naturalWidth;
+  let imageFirstHeight = imageFirst.naturalHeight;
+  let imageFirstType = imageFirst.src.slice(imageFirst.src.indexOf("/") + 1, imageFirst.src.indexOf(";"));
+
+  encryptionData.addPicture(imageFirst.src, imageFirstWidth, imageFirstHeight, imageFirstType, 1);
+
+  let imageSecondWidth = imageSecond.naturalWidth;
+  let imageSecondHeight = imageSecond.naturalHeight;
+  let imageSecondType = imageSecond.src.slice(imageSecond.src.indexOf("/") + 1, imageSecond.src.indexOf(";"));
+  encryptionData.addPicture(imageSecond.src, imageSecondWidth, imageSecondHeight, imageSecondType, 2);
+}
+
+formDecipherInputFileFirst.addEventListener("change", (e) => {
+  uploadImage(e, formDecipherImageFirst);
+  toggleDownloadedPic(formDecipher, 1);
+});
+formDecipherInputFileSecond.addEventListener("change", (e) => {
+  uploadImage(e, formDecipherImageSecond);
+  toggleDownloadedPic(formDecipher, 2);
+  formDecipherImageSecond.onload = function() {
+    declareDataDecipher(formDecipherImageFirst, formDecipherImageSecond);
+  };
+});
